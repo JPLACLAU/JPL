e45d2c0890e73b5ca17a693dfeda12767ae13940
JPLACLAU <laclaujeanpaul@gmail.com>
2023-01-14 18:38:01 -0300

    z

--- src/router/Routes.js
+++ src/router/Routes.js
@@ -11,7 +11,7 @@ const Routes = () => {
       <Router>
         <ScrollTopBehaviour />
         <Switch>
-          <Route exact path="/" component={HomeDarkParticles} />
+          <Route exact path="/JPL" component={HomeDarkParticles} />
         </Switch>
       </Router>
     </>
